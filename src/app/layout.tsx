599992b7d11517a500434d2fdd76dd30d9745c51
KarlStevenRequinto <lerdrobott@gmail.com>
2025-10-15 21:35:15 +0800

    setting global font family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import { Navbar } from "@/components/navigation/Navbar";
 import "./globals.css";
 
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
-});
+
 
 export const metadata: Metadata = {
     title: "Auto Real Estate Ecommerce",
@@ -25,11 +19,12 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={`${geistSans.variable} antialiased`}>
+            <body className={`${inter.variable} antialiased`}>
                 <Navbar />
                 <main className="mx-auto flex min-h-[calc(100vh-5rem)] max-w-5xl flex-col gap-8 px-6 py-10">{children}</main>
             </body>
         </html>
     );
 }
+
 
